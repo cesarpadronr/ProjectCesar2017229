29f75bfa657ecb934e8a240c1a78f9acf80fe46d
cesarpadronr <cesar.padronr@hotmail.com>
2020-04-26 13:39:37 +0100

    creating routes, add/delete user

---
+++ routes/users.js
@@ -0,0 +1,62 @@
+import express from "express";
+import User from "../schema/User"
+import mongoose from "mongoose";
+
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+    res.status(200).json({
+        message:"Serving Users on the Endpoint."
+    });   
+});
+
+router.get("/list", (req, res, next) => {
+    User.find({})
+        .exec()
+        .then(docs => {
+            res.status(200).json({
+                docs
+            });
+        })
+        .catch(err => {
+            console.log(err)
+        });
+});
+//To add the user, we will accept a couple of fields from our application form. It will be passed as JSON data to our endpoint. 
+router.post("/add", (req, res, next) => {
+
+    const user = new User({
+        _id: mongoose.Types.ObjectId(),
+        product: req.body.product,
+        quantity:req.body.quantity,
+        price: req.body.price
+    });
+//We will use ID as an internally generated key.
+    user.save()
+    .then(result => {
+        res.status(200).json({
+            docs:[user]
+        });
+    })
+    .catch(err => {
+        console.log(err);
+    });
+});
+//we will find the User with given ID and if it is found we will remove it.
+router.post("/delete", (req, res, next) => {
+    const rid = req.body.id;
+
+    User.findById(rid)
+        .exec()
+        .then(docs => {
+            docs.remove();
+            res.status(200).json({
+                deleted:true
+            });
+        })
+        .catch(err => {
+            console.log(err)
+        });
+});
+
+module.exports = router;
